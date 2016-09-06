f13f36ef0e078581d6ff4bfe1f24d20e07f6706f
Allen Shaw <allen@JoineryHQ.com>
2016-09-06 18:08:27 -0500

    Removed various in-progress files for bare proof-of-concept.

--- sql/auto_uninstall.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS `civicrm_fbpixel_rule`;
-
-DELETE
-  og.*, ov.*
-FROM
-  civicrm_option_group og
-  INNER JOIN civicrm_option_value ov ON ov.option_group_id = og.id
-WHERE
-  og.name in ('fbpixel_entities', 'fbpixel_pages', 'fbpixel_events', 'fbpixel_actions');	
